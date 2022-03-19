34fc06d9b0af196be7c6f97354ad9af81d37b8a1
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-03-19 09:02:43 +0100

    cd..

---
+++ app/src/main/java/com/com/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.com.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    val counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textview1: TextView = findViewById(R.id.textView1)
+        textview1.setText("Nowy tekst")
+
+        val startButton: Button = findViewById(R.id.button_start)
+        startButton.setOnClickListener {
+            textview1.setText("Kolejny tekst")
+        }
+
+    }
+}
