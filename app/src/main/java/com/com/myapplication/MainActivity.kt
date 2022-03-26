b43d5c52fed6a1ade91ebd1147d1979683dfb7d5
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-03-26 09:29:54 +0100

    cd..

--- app/src/main/java/com/com/myapplication/MainActivity.kt
+++ app/src/main/java/com/com/myapplication/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
             Toast.makeText(this, "Naciśnięto start", Toast.LENGTH_SHORT).show()
-
         }
 
         mainLayout.setOnClickListener {
