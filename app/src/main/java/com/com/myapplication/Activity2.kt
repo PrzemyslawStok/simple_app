55c906452b81c6add65863003a0700d1a96c3d26
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-23 08:42:12 +0200

    cd..

--- app/src/main/java/com/com/myapplication/Activity2.kt
+++ app/src/main/java/com/com/myapplication/Activity2.kt
@@ -1,11 +1,17 @@
 package com.com.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.com.myapplication.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity2)
+        //setContentView(R.layout.activity2)
+
+        val binding = Activity2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
