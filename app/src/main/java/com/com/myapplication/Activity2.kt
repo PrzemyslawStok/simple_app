377c1ea6f288d1d2472122c1cda96c1941f29b2c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-23 09:24:19 +0200

    cd..

--- app/src/main/java/com/com/myapplication/Activity2.kt
+++ app/src/main/java/com/com/myapplication/Activity2.kt
@@ -2,6 +2,7 @@ package com.com.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.com.myapplication.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
@@ -15,5 +16,22 @@ class Activity2 : AppCompatActivity() {
         val view1a = binding.view1a
         val view1b = binding.view1b
 
+        val colorList = arrayListOf(
+            R.color.view1a,
+            R.color.view1b,
+            R.color.view1c,
+            R.color.view2a,
+            R.color.view2b,
+            R.color.view2c,
+            R.color.view3a,
+            R.color.view3b,
+            R.color.view3c
+        ).map { ContextCompat.getColor(this, it) }
+
+
+        view1a.setOnClickListener {
+            val color = colorList[8]
+            view1a.setBackgroundColor(color)
+        }
     }
 }
